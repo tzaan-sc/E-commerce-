f8fa2a2bacbac0692e2a0a8d3574ee8f6f672402
tzaan-sc <tv.ntv.ntvan@gmail.com>
2025-09-12 10:38:40 +0700

    First test BE FE

---
+++ backend/backend/src/main/java/com/demo/backend/HelloController.java
@@ -0,0 +1,12 @@
+package com.demo.backend;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Hello from Spring Boot 🚀";
+    }
+}
